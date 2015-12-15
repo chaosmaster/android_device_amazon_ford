89097e9a3b618952765795310392db3b5228d374
Chaosmaster <chaosmaster@chasov.de>
2015-12-15 18:17:46 +0100

    fixed build-error
    Change-Id: I9015e7411972160db48d6ead95c6d36ffc37cc19

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := user
+LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES += bootable/recovery
 LOCAL_C_INCLUDES += system/core/fs_mgr/include
 LOCAL_SRC_FILES := default_device.cpp
